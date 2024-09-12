d83293c3a6a4a406cf7ca2c7d6aec6b911a99ebe
Ufuk Baran Güler <baran.guler@ug.bilkent.edu.tr>
2024-09-12 15:36:06 +0300

    add main file and created some variables

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int totalKilometersDrivenPerDay{0};
+    int costPerLiterOfGasoline{0};
+    int averageKilometersPerLiter{0};
+    int parkingFeesPerDay{0};
+    int tollsPerDay{0};
+
+
+    return 0;
+}

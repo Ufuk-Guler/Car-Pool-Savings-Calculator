fca5d43e626f15ebfb1a42abff4766c57fab865b
Ufuk Baran Güler <baran.guler@ug.bilkent.edu.tr>
2024-09-12 15:52:30 +0300

    complete main

--- main.cpp
+++ main.cpp
@@ -7,7 +7,27 @@ int main() {
     int averageKilometersPerLiter{0};
     int parkingFeesPerDay{0};
     int tollsPerDay{0};
+    int dailyDrivingCost{0};
 
+    cout << "Enter total kilometers driven per day: ";
+    cin >> totalKilometersDrivenPerDay;
+
+    cout << "Enter cost per liter of gasoline: ";
+    cin >> costPerLiterOfGasoline;
+
+    cout << "Enter average kilometers per liter: ";
+    cin >> averageKilometersPerLiter;
+
+    cout << "Enter parking fees per day: ";
+    cin >> parkingFeesPerDay;
+
+    cout << "Enter tolls per day: ";
+    cin >> tollsPerDay;
+
+    dailyDrivingCost = tollsPerDay + parkingFeesPerDay +
+            (totalKilometersDrivenPerDay / averageKilometersPerLiter) * costPerLiterOfGasoline;
+
+    cout << "You daily driving cost is " << dailyDrivingCost << endl;
 
     return 0;
 }
